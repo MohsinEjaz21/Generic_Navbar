3ce3b8813faca3e44ce0933fcc393abeaf87c2eb
Mohsin Ejaz <mejaz.bese21@gmail.com>
2022-10-11 19:39:24 +0500

    adding icons

--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -107,7 +107,7 @@ function index() {
   return (
     <header className="header-sec">
       <img className="logo" src="https://www.kyndryl.com/content/experience-fragments/kyndrylprogram/us/en/sites/header/master/_jcr_content/root/header_copy/image.coreimg.svg/1636019574172/kyndryl-logo.svg" alt="Kyndryl logo" />
-      <nav className="navbar slideInDown" ref={navRef}>
+      <nav className="navbar" ref={navRef}>
         {data.menu.map((e, i) => (
           <div className="nav__item" tabIndex={i}>
             <CreateLink text={e.text}
